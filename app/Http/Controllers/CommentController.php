061746877034a7a8d930ce560ec338da594c9e47
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-02-20 20:45:00 +0100

    comments created

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CommentController extends Controller
+{
+    public function store(StoreCommentRequest $request, Post $post)
+    {
+
+         if (!Auth::check()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $comment = new Comment([
+            'content' => $request->input('content'),
+            'user_id' => Auth::id(),
+        ]);
+
+        $post->comments()->save($comment);
+
+        return response()->json(['message' => 'Comment created successfully', 'comment' => $comment], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,4 +16,7 @@ Route::apiResource('posts', PostController::class);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->post('/posts/{post}/comments', [CommentController::class, 'store']);
+
 
