f02f100c9780cb27a14d1ab51f57978489e88968
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-02-26 20:52:01 +0100

    Private and Public routes updated

--- routes/api.php
+++ routes/api.php
@@ -6,17 +6,27 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('posts', PostController::class);
+
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+
+});
 
 
-Route::apiResource('posts', PostController::class);
+//Public Routes
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->post('/posts/{post}/comments', [CommentController::class, 'store']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 
