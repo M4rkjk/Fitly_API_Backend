6a76a47f99e25ed60c3796e20f577123ee5664b3
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-03-03 18:59:10 +0100

    Post model new connection to PostReactions

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -13,7 +14,7 @@ class Post extends Model
     protected $fillable = [
         'title',
         'content',
-        'image_path', 
+        'image_path',
     ];
 
     public function user()
@@ -29,5 +30,15 @@ class Post extends Model
     public function getImageUrlAttribute()
     {
         return $this->image_path ? asset('storage/' . $this->image_path) : null;
+    }
+
+    /**
+     * Get all of the postReactions for the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function postReactions(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
