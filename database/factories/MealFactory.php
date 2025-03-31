ec5556a6e94b939c310f41e992e961d7a7e43612
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-03-31 19:54:52 +0200

    fake data generation for ingredients and meals

--- database/factories/MealFactory.php
+++ database/factories/MealFactory.php
@@ -17,7 +17,13 @@ class MealFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'kcal' => $this->faker->randomFloat(2, 50, 1000),
+            'fat' => $this->faker->randomFloat(2, 1, 100),
+            'carb' => $this->faker->randomFloat(2, 1, 100),
+            'protein' => $this->faker->randomFloat(2, 1, 100),
+            'salt' => $this->faker->randomFloat(2, 0, 20),
+            'sugar' => $this->faker->randomFloat(2, 0, 50),
         ];
     }
 }
